db6bd0417cc51975a7b4b01994884750a849f38b
FahmidaTasmia <tasmia1070@gmail.com>
2022-09-03 22:35:09 +0600

    blog site added

--- js/app.js
+++ js/app.js
@@ -1,55 +1,54 @@
-const loadCategory = async()=>{
-const url = `https://openapi.programming-hero.com/api/news/categories`
-const res = await fetch(url);
-const data = await res.json();
-displayCategory(data.data.news_category)
+const loadCategory = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategory(data.data.news_category)
 }
 
-const displayCategory = allnews =>{
+const displayCategory = allnews => {
   const categoryNews = document.getElementById('category-news');
-allnews.forEach(news => {
-  const categoryDiv = document.createElement('div');
-  categoryDiv.classList.add('col')
-  categoryDiv.classList.add('user')
-  categoryDiv.innerHTML=`
+  allnews.forEach(news => {
+    const categoryDiv = document.createElement('div');
+    categoryDiv.classList.add('col')
+    categoryDiv.classList.add('user')
+    categoryDiv.innerHTML = `
   <button onclick ="loadNews('${news.category_id}')"> ${news.category_name} </button>
   `;
-  categoryNews.appendChild(categoryDiv);
-});
+    categoryNews.appendChild(categoryDiv);
+  });
 }
 
 loadCategory()
 
 //loader-spinner 
-const loadSpinner = isLoading =>{
+const loadSpinner = isLoading => {
   const loader = document.getElementById('loader');
-  if(isLoading){
+  if (isLoading) {
     loader.classList.remove('d-none')
-  }
-  else{
+  } else {
     loader.classList.add('d-none')
   }
 }
 
 //show all news details
- const loadNews = async(id) =>{
+const loadNews = async (id) => {
   loadSpinner(true);
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayNews(data.data))
- }
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayNews(data.data))
+}
 
 
- const displayNews = categoryId =>{
+const displayNews = categoryId => {
   const displayAllNews = document.getElementById('display-all-news');
-  displayAllNews.innerText=categoryId.length;
+  displayAllNews.innerText = categoryId.length;
   const newsContainer = document.getElementById('news-container');
   newsContainer.innerHTML = '';
-  categoryId.forEach(id=>{
+  categoryId.forEach(id => {
     const newsDiv = document.createElement('div');
     newsDiv.classList.add('col');
-    newsDiv.innerHTML=`
+    newsDiv.innerHTML = `
     <div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
@@ -79,18 +78,18 @@ const loadSpinner = isLoading =>{
 
   })
   loadSpinner(false)
- }
+}
 
 
- const loadModal = news_id =>{
+const loadModal = news_id => {
   const url = `https://openapi.programming-hero.com/api/news/${news_id}`
   fetch(url)
-  .then(res => res.json())
-  .then(data => displayModal(data.data))
+    .then(res => res.json())
+    .then(data => displayModal(data.data))
 }
-const displayModal = data =>{
- 
-  data.forEach(news =>{
+const displayModal = data => {
+
+  data.forEach(news => {
     console.log(news)
     const modalTitle = document.getElementById('exampleModalLabel')
     modalTitle.innerText = news.title;
@@ -101,9 +100,8 @@ const displayModal = data =>{
     <P> Name: ${news.author.name ? news.author.name : 'No found name'} </br> published date: ${news.author.published_date}
     <hr>
     <p><i class="fa-solid fa-eye"></i> ${news.total_view ? news.total_view : 'No data available'} </p>
-    <p> Title: ${news.title}</p>
+    <p> Tit le: ${news.title}</p>
     <img class="img-fluid" src="${news.thumbnail_url}" alt="">
     `
-  })  
+  })
 }
-
